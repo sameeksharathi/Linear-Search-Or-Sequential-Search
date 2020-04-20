466bc6ad3afe741c77670380e496864b689fedce
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 20:44:51 +0530

    main class created

---
+++ LargestAndSmallestElementMain.java
@@ -0,0 +1,14 @@
+class LargestAndSmallestElementMain{
+    public static void main(String[] args) {
+        LargestAndSmallestElementInArray obj=new LargestAndSmallestElementInArray();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 5 values in the array:");
+        for (int i = 0; i <obj.arr.length ; i++) {
+            obj.arr[i]=sc.nextInt();
+        }
+        System.out.print("Largest element from the array: ");
+        obj.largestelement();
+        System.out.print("Smallest element from the array: ");
+        obj.smallestelement();
+    }
+}
