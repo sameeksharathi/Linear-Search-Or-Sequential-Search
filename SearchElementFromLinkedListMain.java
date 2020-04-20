e4b52d260c1e5ca91a46627114e1de62513fb0c8
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 20:55:19 +0530

    created

---
+++ SearchElementFromLinkedListMain.java
@@ -0,0 +1,12 @@
+class SearchElementFromLinkedListMain{
+    public static void main(String[] args) {
+        SearchElementFromLinkedList obj=new SearchElementFromLinkedList();
+        obj.insert(20);
+        obj.insert(30);
+        obj.insert(40);
+        obj.insert(60);
+        obj.insert(90);
+        obj.display();
+        obj.search(30);
+    }
+}
